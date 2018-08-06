17d74f9f70c930ad08e2e06d7312be2d0d1ed9a5
Chris Lipscombe <btg2757@autuni.ac.nz>
2018-08-07 11:34:41 +1200

    Re-start 2018
    Fixing my life. Repulling to restart QR code.

--- Tessera/app/src/main/java/com/example/blake/tessera/MainActivity.java
+++ Tessera/app/src/main/java/com/example/blake/tessera/MainActivity.java
@@ -11,21 +11,9 @@ import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
-
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-    SurfaceView surfaceView;
-    CameraSource cameraSource;
-    TextView textView;
-    BarcodeDetector barcodeDetector;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,60 +24,6 @@ public class MainActivity extends AppCompatActivity {
 //       hamburger.setBackground(getResources().getDrawable(R.color.colorAccent));
 
        setSupportActionBar(hamburger);
-
-       surfaceView = (SurfaceView) findViewById(R.id.cameraPreview);
-       barcodeDetector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
-       textView = (TextView) findViewById(R.id.textView);
-       cameraSource = new CameraSource.Builder(this, barcodeDetector).setRequestedPreviewSize(640,480).build();
-
-       surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
-           @Override
-           public void surfaceCreated(SurfaceHolder holder) {
-               if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                   return;
-               }
-               try{
-                   cameraSource.start(holder);
-               }catch(IOException e){
-
-               }
-           }
-
-           @Override
-           public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-           }
-
-           @Override
-           public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                cameraSource.stop();
-           }
-       });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrCode = detections.getDetectedItems();
-
-                if(qrCode.size() != 0){
-                    textView.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-
-                            textView.setText(qrCode.valueAt(0).displayValue);
-                        }
-                    });
-                }
-            }
-        });
-
 
     }
 }
