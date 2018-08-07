c64ae8c088ebf13fc5f52944a8171437d6584847
Chris Lipscombe <btg2757@autuni.ac.nz>
2018-08-07 13:51:15 +1200

    Cover Page
    Cover page done, able to swap pages

--- Tessera/app/src/main/java/com/example/blake/tessera/MainActivity.java
+++ Tessera/app/src/main/java/com/example/blake/tessera/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.blake.tessera;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Vibrator;
 import android.support.v4.app.ActivityCompat;
@@ -10,10 +11,14 @@ import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signInToAut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
        setSupportActionBar(hamburger);
 
+
+        signInToAut = (Button) findViewById(R.id.signInToAut);
+
+        signInToAut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, NorthCampus.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
